3a9e8b44a3632b37ee2a637bc61b58e1424065bf
Ian França <77856752+Ian-FS@users.noreply.github.com>
2023-08-28 20:27:45 +0000

    manipulando props com typeScript

---
+++ React-typescript/src/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type ButtonProps = React.ComponentProps<'button'> & {
+    tamanho?: string
+}
+
+const Button = ({tamanho, ...props}: ButtonProps) => {
+    return (
+        <button
+            style={{fontSize: tamanho}}
+            {...props}
+
+        />
+    )
+}
+
+export default Button;
