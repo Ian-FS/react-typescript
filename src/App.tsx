f9d565f8fd2a423e754757027caee34cd67a2207
Ian_França <iiann_2009@hotmail.com>
2023-09-03 12:00:14 -0300

    atualizado exercicio que consome uma api de vendas

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,57 @@
-import React, { HtmlHTMLAttributes, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "./Input";
 
 function App() {
-  type dados = {
+  type venda = {
+    id: string;
     nome: string;
     status: string;
   };
 
-  const [data, setData] = useState({
-    inicio: "2023/06/09",
-    fim: "2023/06/15",
-  });
-  const [isChangeInicial, setIsChangeInicial] = useState(false);
-  const [isChangeFinal, setIsChangeFinal] = useState(false);
-
-  const trataString = (string: string) => (string = string.replace("-", "/"));
+  const [data, setData] = useState<null | venda[]>(null);
+  const [dataInicial, setDataInicial] = useState("");
+  const [dataFinal, setDataFinal] = useState("");
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     if (event.currentTarget.id === "inicio") {
-      setData({ ...data, inicio: trataString(event.currentTarget.value) });
-      console.log(data);
-
-      setIsChangeInicial(!isChangeInicial);
-      console.log(data);
+      setDataInicial(event.currentTarget.value);
+      console.log(dataInicial);
     }
     if (event.currentTarget.id === "final") {
-      setData({ ...data, fim: trataString(event.currentTarget?.value) });
-      setIsChangeFinal(!isChangeFinal);
-      console.log(data);
+      setDataFinal(event.currentTarget.value);
+      console.log(dataFinal);
     }
   };
 
   useEffect(() => {
-    fetch(
-      `https://data.origamid.dev/vendas/?inicio=2023/06/14&final=2023/06/15`
-    )
-      .then((res) => res.json())
-      .then((dados: object[]) => {
-        dados.forEach((dado: dados) => {
-          console.log(dado.nome, dado.status);
+    if (dataInicial !== "" && dataFinal !== "")
+      fetch(
+        `https://data.origamid.dev/vendas/?inicio=${dataInicial}&final=${dataFinal}`
+      )
+        .then((res) => res.json())
+        .then((dados: venda[]) => {
+          dados.forEach((dado) => {
+            console.log(`${dado.nome}: ${dado.status}`);
+          });
         });
-      });
-  }, [isChangeFinal, isChangeInicial]);
+  }, [dataInicial, dataFinal]);
 
   return (
     <div>
-      <Input id="inicio" label="Inicio" type="date" onChange={handleChange} />
-      <Input id="final" label="Fim" type="date" onChange={handleChange} />
+      <div>
+        <Input id="inicio" label="Inicio" type="date" onChange={handleChange} />
+        <Input id="final" label="Fim" type="date" onChange={handleChange} />
+      </div>
+      <div>
+        <ul>
+          {data &&
+            data.map((venda) => {
+              <li key={venda.id}>
+                {venda.nome}: ${venda.status}
+              </li>;
+            })}
+        </ul>
+      </div>
     </div>
   );
 }
