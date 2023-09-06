210da375ea2a6e112960f940fe6f35e261693eaf
Ian_França <iiann_2009@hotmail.com>
2023-09-06 03:32:28 -0300

    utilizando useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { Input } from "./Input";
-
-type venda = {
-  id: string;
-  nome: string;
-  status: string;
-};
+import { useRef } from "react";
+import Button from "./Button";
 
 function App() {
-  const [dados, setDados] = React.useState<null | venda[]>(null);
-  const [dataInicial, setDataInicial] = useState("");
-  const [dataFinal, setDataFinal] = useState("");
-
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    if (event.currentTarget.id === "inicio") {
-      setDataInicial(event.currentTarget.value);
-      console.log(dataInicial);
-    }
-    if (event.currentTarget.id === "final") {
-      setDataFinal(event.currentTarget.value);
-      console.log(dataFinal);
-    }
-  };
-
-  useEffect(() => {
-    if (dataInicial !== "" && dataFinal !== "")
-      fetch(
-        `https://data.origamid.dev/vendas/?inicio=${dataInicial}&final=${dataFinal}`
-      )
-        .then((res) => res.json())
-        .then((dados) => setDados(dados as venda[]))
-        .catch((error) => console.log(error));
-  }, [dataInicial, dataFinal]);
+  const video = useRef<HTMLVideoElement>(null);
 
   return (
     <div>
-      <div>
-        <Input
-          id="inicio"
-          label="Inicio"
-          type="date"
-          onChange={handleChange}
-          value={dataInicial}
-        />
-        <Input
-          id="final"
-          label="Fim"
-          type="date"
-          onChange={handleChange}
-          value={dataFinal}
-        />
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Button children={"Play"} onClick={() => video.current?.play()} />
+        <Button children={"Pause"} onClick={() => video.current?.pause()} />
       </div>
-      <ul>
-        {dados?.map((dado) => (
-          <li key={dado.id}>
-            {dado.nome}: {dado.status}
-          </li>
-        ))}
-      </ul>
+      <video
+        ref={video}
+        style={{ width: "50%" }}
+        src="../src/assets/video.mp4"
+        controls
+      ></video>
     </div>
   );
 }
