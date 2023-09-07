830d6b137958ed6c0297649dabe461774a9a9abb
Ian_França <iiann_2009@hotmail.com>
2023-09-07 02:31:18 -0300

    criado botão que dar Play e Pausa video

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Button from "./Button";
 
 function App() {
   const video = useRef<HTMLVideoElement>(null);
+  const [isMuted, setIsMuted] = useState(false);
+  const [isPlay, setIsPlay] = useState(true);
 
   return (
     <div>
       <div style={{ display: "flex", gap: "10px" }}>
-        <Button children={"Play"} onClick={() => video.current?.play()} />
+        <Button
+          children={isPlay ? "Play" : "Pause"}
+          onClick={() => {
+            isPlay ? video.current?.play() : video.current?.pause();
+            setIsPlay(!isPlay);
+          }}
+        />
         <Button children={"Pause"} onClick={() => video.current?.pause()} />
+        <Button children={"Mute"} onClick={() => setIsMuted(() => !true)} />
       </div>
       <video
         ref={video}
         style={{ width: "50%" }}
         src="../src/assets/video.mp4"
         controls
+        muted={isMuted}
       ></video>
     </div>
   );
