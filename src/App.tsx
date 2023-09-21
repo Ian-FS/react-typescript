f980bee92106efeab27aedf3142984a69f259276
Ian_França <iiann_2009@hotmail.com>
2023-09-21 14:41:11 -0300

    useReducer inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { Content } from "./Content";
-import Header from "./Header";
-import { MyFetchContextProvider } from "./fetchContext";
+import { useReducer } from "react";
+import { Input } from "./Input";
+
+type State = {
+  nome: string;
+  email: string;
+};
+
+type Action = { setNome: string; setEmail: string };
 
 function App() {
+  function reducer(state: State, action: Action) {
+    return state;
+  }
+
+  const [state, dispatch] = useReducer(reducer, { nome: "", email: "" });
+
   return (
-    <MyFetchContextProvider>
-      <Header />
-      <Content />
-    </MyFetchContextProvider>
+    <div>
+      <h1>Formulário:</h1>
+      <Input
+        label="Nome:"
+        id="name"
+        style={{ marginBottom: "10px" }}
+        // onChange={(target) => dispatch()}
+      />
+      <Input label="Email:" id="email" />
+    </div>
   );
 }
 
